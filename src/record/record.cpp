6f22e6732802ab07a19acbe860458f963fc4cf60
Player/Coach <olafinski14@gmail.com>
2024-11-17 13:40:01 +0100

    First version of record type

--- src/record/record.cpp
+++ src/record/record.cpp
@@ -1,11 +1,77 @@
 #include "record.h"
 
-Record::Record(/* args */)
+Record::Record()
 {
-    std::cout << "Record::Record()" << std::endl;
+    for (int i = 0; i < RECORD_SERIES_LENGTH; i++)
+    {
+        std::srand(time(NULL));
+        for(int i = 0; i < RECORD_SERIES_LENGTH; i++)
+        {
+            this->series[i] = MIN_NUMBER_VALUE + std::rand() % (MAX_NUMBER_VALUE - MIN_NUMBER_VALUE + 1);
+        }
+    }
 }
 
-Record::~Record()
+Record::~Record(int series[RECORD_SERIES_LENGTH])
+{
+    for (int i = 0; i < RECORD_SERIES_LENGTH; i++)
+    {
+        this->series[i] = series[i];
+    }
+}
+
+const Record &Record::operator[](int index) const
+{
+    return this->series[index];
+}
+
+Record &Record::operator[](int index)
+{
+    return this->series[index];
+}
+
+Record &Record::operator=(const Record &record)
+{
+    for (int i = 0; i < RECORD_SERIES_LENGTH; i++)
+    {
+        this->series[i] = record.series[i];
+    }
+    return *this;
+}
+
+const int* getRecord()
+{
+    return this->series;
+}
+
+Record::operator<(const Record &record) const
+{
+    int maxA = 0;
+    int maxB = 0;
+    bool initialMaxSet = false;
+    std::vector<int> bCopy(std::begin(record.getSeries()), std::end(record.getSeries()));
+    for(int i=0; i<RECORD_SERIES_LENGTH; i++)
+    {
+        for(int j=0; j<RECORD_SERIES_LENGTH; j++)
+        {
+            if(this[i] == record[j])
+            {
+                bCopy.erase(std::remove(bCopy.begin(), bCopy.end(), num), bCopy.end());
+                break;
+            }
+            if(this[i] > maxA || initialMaxSet == false)
+            {
+                initialMaxSet = true;
+                maxA = this[i];
+            }
+        }
+    }
+    maxB = std::max_element(bCopy.begin(), bCopy.end());
+    if(maxA < maxB)
+        return true;
+    return false;
+}
+
 {
     std::cout << "Record::~Record()" << std::endl;
 }
--- src/record/record.h
+++ src/record/record.h
@@ -1,9 +1,26 @@
 #include <iostream>
+#include "const.h"
+#include <cstdlib>
+#include <time.h>
+#include <vector>
 class Record
 {
 private:
-    /* data */
+    int series[RECORD_SERIES_LENGTH];
 public:
-    Record(/* args */);
+    Record();
+    Record(int series[RECORD_SERIES_LENGTH]);
+    const Record& operator [] (int index) const;
+    Record& operator [] (int index);
+    const int* getSeries() const;
+    Record& operator = (const Record &record);
+    bool operator < (const Record &record) const;
+    bool operator > (const Record &record) const;
+    bool operator == (const Record &record) const;
+    bool operator != (const Record &record) const;
+    bool operator <= (const Record &record) const;
+    bool operator >= (const Record &record) const;
+    friend std::ostream &operator << (std::ostream &os, const Record &record);
+    friend std::istream &operator >> (std::istream &is, Record &record);
     ~Record();
 };
